7647303323977333c2ed3ce335708e2ac1ce98a1
MuXiang <110757121+xiang0105@users.noreply.github.com>
2026-05-29 01:09:11 +0800

    Add basic Google Maps screen

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Flutter
+import GoogleMaps
 import UIKit
 
 @main
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCz-h9r0-ON76xgA2DVx2VfwDkAnyVHgFw")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
